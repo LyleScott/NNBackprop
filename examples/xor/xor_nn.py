ed37448888e6bec2625f96457bde8a13923792cc
Lyle Scott III <lyle@digitalfoo.net>
2011-09-06 01:54:31 -0400

    xor example updates

--- examples/xor/xor_nn.py
+++ examples/xor/xor_nn.py
@@ -11,6 +11,7 @@ def main():
 
     # create a simple 2x2x1 network.
     nnet = NN.NeuralNetwork(name='Example XOR Network')
+    nnet.set_learning_rate(0.4)
     nnet.create_network_architecture(n_inputs=2, n_hiddens=2, n_outputs=1)
 
     # train the network
@@ -19,13 +20,17 @@ def main():
     nnet.training_loop(tsse_threshold=.005)
     #nnet.training_loop(epochs_threshold=9999)
 
+    print '-'*80
+
     # test the network
     # - input values that are NOT xor'ed should put the output value 'low'
     # - input values that ARE xor'ed should put the output value 'high'
     for vector in [[0,0], [0,1], [1,0], [1,1]]:
         nnet.set_inputs([vector[0], vector[1]])
         nnet.feed_forward()
-        print 'NN output:', nnet.get_outputs()[0]
+        print 'NN test:', vector,  '==', nnet.get_outputs()[0]
+
+    nnet.print_network_state()
 
     # plot the activation due to a bunch of numbers 0-1
     # 1) uncomment save_network_state and save_network_state_iteration_modulo 
--- src/NNBackprop/Components/Links.py
+++ src/NNBackprop/Components/Links.py
@@ -8,16 +8,16 @@ import random
 class NeuronLink():
     def __init__(self, in_index, out_index, layer_type):
         self.layer_type = layer_type
-        self.in_index   = in_index               # index of input node
-        self.out_index  = out_index              # index of output node
-        self.weight     = random.uniform(-1, 1)  # floating point [-1, 1]
+        self.in_index   = in_index
+        self.out_index  = out_index
+        self.weight     = random.uniform(-.5, .5)
         
     def __str__(self):
         weight = str(round(self.weight,12))
         if self.layer_type == "hidden":
             return ''.join(['Input', str(self.in_index),
-                          ' <---------- \t' + weight + '\t --------> ',  
-                          'Hidden', str(self.out_index)])
+                            ' <---------- \t' + weight + '\t --------> ',  
+                            'Hidden', str(self.out_index)])
         elif self.layer_type == "output":
             return ''.join(['Hidden', str(self.in_index),
                             ' <--------- \t', weight, '\t --------> ',
